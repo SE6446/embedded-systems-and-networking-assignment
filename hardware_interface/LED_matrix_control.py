200704bc9e3d24b196a40a0bd03d0603fa4e7d49
Archie Macdonald <archiemac07@outlook.com>
2025-11-10 12:55:22 +0000

    Made and tested the board, some oddities need to be cleared out.
    - There are some funky effects when using amber colours and changing colours

--- hardware_interface/LED_matrix_control.py
+++ hardware_interface/LED_matrix_control.py
@@ -2,29 +2,29 @@ from machine import Pin
 from utime import sleep_ms as sleep  # pyright: ignore[reportUnknownVariableType]
 
 row_pins: list[Pin] = [
-    Pin(2,Pin.OUT), # Row 1
-    Pin(3,Pin.OUT), # Row 2
-    Pin(4,Pin.OUT), # Row 3 
+    Pin(0,Pin.OUT), # Row 1
+    Pin(1,Pin.OUT), # Row 2
+    Pin(2,Pin.OUT), # Row 3 
 ]
 
 col_pins: list[Pin] = [
-    Pin(5, Pin.OUT),  # Col 1 Red
-    Pin(6, Pin.OUT),  # Col 1 Green
-    Pin(7, Pin.OUT),  # Col 2 Red
-    Pin(8, Pin.OUT),  # Col 2 Green
-    Pin(9, Pin.OUT),  # Col 3 Red
-    Pin(10, Pin.OUT)  # Col 3 Green
+    Pin(3, Pin.OUT),  # Col 1 Red
+    Pin(4, Pin.OUT),  # Col 1 Green
+    Pin(5, Pin.OUT),  # Col 2 Red
+    Pin(6, Pin.OUT),  # Col 2 Green
+    Pin(7, Pin.OUT),  # Col 3 Red
+    Pin(8, Pin.OUT)  # Col 3 Green
 ]
 
 
 def clear_all_rows() -> None:
     """Clear all the rows to prevent LED ghosting"""
     for row in row_pins:
-        row.high()
+        row.low()
 
 def clear_all_columns() -> None:
     for column in col_pins:
-        column.low()
+        column.high()
     
 def clear_matrix() -> None:
     clear_all_rows()
@@ -50,41 +50,49 @@ def update_matrix(matrix:list[list[int]]) -> None:
     [1,2,1]] # red, green, red
     
     """
-
+    clear_matrix()
     # matrix validation
     if len(matrix) != 3 or len(matrix[0]) != 3:
         raise Exception(f"Parameter error: matrix shape is out of bounds. Number of rows: {len(matrix)}.\nNumber of columns: {len(matrix[0])}.\nExpected 3x3.")
 
     for i in range(3):
+        activated = False
         for j in range(3):
+            
             state: int = matrix[i][j]
-
-            red_pin: Pin = col_pins[j*2]
-            green_pin: Pin = col_pins[j*2+1]
+            red_pin: Pin = col_pins[j*2+1]
+            green_pin: Pin = col_pins[j*2]
+            """Pins are common anode, so setting to low turns on the LED"""
 
             #The five possible states
             #OFF
             if state == 0:
-                red_pin.low()
-                green_pin.low()
+                red_pin.high()
+                green_pin.high()
+                
             #RED
             elif state==1:
-                red_pin.high()
-                green_pin.low()
-            #GREEN
-            elif state == 2:
                 red_pin.low()
                 green_pin.high()
+                activated = True
+            #GREEN
+            elif state == 2:
+                red_pin.high()
+                green_pin.low()
+                activated = True
             #AMBER
             elif state == 3:
-                red_pin.high()
-                green_pin.high()
+                red_pin.low()
+                green_pin.low()
+                activated = True
             #FUCK
             else:
                 clear_matrix()
                 raise Exception(f"Invalid state: Expected a value between 0 and 3. Got {state}")
-        #We set this to low so that the ground pins activate and we get current
-        row_pins[i].low()
+        # 
+        if activated:
+            #We set this to high so that the ground pins activate and we get current
+            row_pins[i].high()
 
         #We wait a moment otherwise the world will explode! ...Or the LEDs will flicker.
         sleep(2)
@@ -92,7 +100,16 @@ def update_matrix(matrix:list[list[int]]) -> None:
 if __name__ == "__main__":
     try:
         while True:
-            update_matrix([[1,2,1],[0,3,0],[1,2,1]])
+            update_matrix([
+                [1,2,1],
+                [0,3,0],
+                [1,2,1]])
+            sleep(3000)
+            update_matrix([
+                [1,0,1],
+                [2,3,2],
+                [1,0,1]])
+            sleep(3000)
     except KeyboardInterrupt:
         clear_matrix()
 
