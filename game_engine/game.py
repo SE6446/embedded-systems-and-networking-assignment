d2dfbbd3282dd5f89415722ac0ac5fe70111051c
Adam M <Thirtynine363@gmail.com>
2025-11-10 14:10:02 +0000

    Started on making a part so that wins and losses can be added to a file

--- game_engine/game.py
+++ game_engine/game.py
@@ -102,6 +102,83 @@ class Game:
             if i < 2:
                 print("-" * 9)
 
+class InfoSaving:
+
+    # copied over from server code
+    # each record will be an object of this class
+    class Entry(object):
+        name:str
+        wins:int
+        losses:int
+
+    # turn file into an useable list of objects
+    # turn a text file with each record split by a line and the data split by commas into an array of Entry
+    def readFile(textFile:str):
+        entries = []
+
+        textFile = open(textFile, "r")
+        
+        # get each line as an individual string in a list
+        lines = textFile.read().split('\n')
+
+        # initialise the array in which the individual data will be stored
+        data = []
+        
+        # for every line of the text fine
+        for i in lines:
+            # split up the line using commas as the separation mark
+            data.append(i.split(','))
+        
+        # initialise counter for getting the right column of data in the text file
+        counter = 0
+
+        # for each line
+        for i in data:
+            # create a new entry for a temporary store the information
+            newEntry = Entry()
+
+            # for each piece of data
+            for j in i:
+                # reset counter so that it does not go out of range of the array (> number is the amount of columns - 1)
+                if (counter > len(i) - 1):
+                    counter = 0
+            
+                # check counter position for column
+                if (counter == 0):
+                    newEntry.name = j
+                elif counter == 1:
+                    newEntry.wins = int(j)
+                else:
+                    newEntry.losses = int(j)
+                # if any more columns are added then more elif statements need added
+                
+                counter = counter + 1
+            
+            # append new entry (row of data) to the list
+            entries.append(newEntry)
+
+        return entries
+    
+    def checkForExistingPlayer(entryList, name:str):
+        # to keep the position in the array
+        counter:int = 0
+
+        #for every entry
+        for i in entryList:
+            
+            # go through every entry until a match is found
+            if i.name == name:
+
+                # return the position in the array
+                return counter
+            counter = counter + 1
+
+        # -1 being returned means that there was not a matching record found in the array 
+        return -1
+
+
+    
+
 
 # Adding a test game into __main__ for testing purposes, this won't run when imported
 if __name__ == "__main__":
