baae8ab09a31b67dbe10e547c4e7fe5243b0c2cd
Adam M <Thirtynine363@gmail.com>
2025-11-13 13:54:30 +0000

    Can now add new users to the leaderboard file

--- game_engine/game.py
+++ game_engine/game.py
@@ -102,179 +102,50 @@ class Game:
             if i < 2:
                 print("-" * 9)
 
-class InfoSaving:
-    textFileDirectory:str = ""
-    
-    def __init__(self) -> None:
-        # Default initialisation for completeness
-        self.textFileDirectory = ""
-
-    # copied over from server code
-    # each record will be an object of this class
-    class Entry:
-        name:str
-        wins:int
-        losses:int
-        def __init__(self) -> None:
-            # Default initialisation for completeness
-            self.name = ""
-            self.wins = 0
-            self.losses = 0
-
-        def __str__(self) -> str:  # pyright: ignore[reportImplicitOverride]
-            return f"Name: {self.name}, Wins: {self.wins}, Losses: {self.losses}"
-
-    # turn a text file with each record split by a line and the data split by commas into an array of Entry
-    def readFile(self):
-        entries:list[InfoSaving.Entry] = []
-
-        fileWrapper = open(InfoSaving.textFileDirectory, "r")
-        
-        # get each line as an individual string in a list
-        lines = fileWrapper.read().split('\n')
-
-        fileWrapper.close() #Remember to close the file after reading!
-
-        # initialise the array in which the individual data will be stored
-        data: list[list[str]] = []
-        
-        # for every line of the text fine
-        for i in lines:
-            # split up the line using commas as the separation mark
-            data.append(i.split(','))
-        
-        # initialise counter for getting the right column of data in the text file
-        counter = 0
-
-        # for each line
-        for i in data:
-            # create a new entry for a temporary store the information
-            newEntry:InfoSaving.Entry = InfoSaving.Entry()
-
-            # for each piece of data
-            for j in i:
-                # reset counter so that it does not go out of range of the array (> number is the amount of columns - 1)
-                if (counter > len(i) - 1):
-                    counter = 0
-            
-                # check counter position for column
-                if (counter == 0):
-                    newEntry.name = j
-                elif counter == 1:
-                    newEntry.wins = int(j)
-                else:
-                    newEntry.losses = int(j)
-                # if any more columns are added then more elif statements need added
-                
-                counter = counter + 1
-            
-            # append new entry (row of data) to the list
-            entries.append(newEntry)
-
-        return entries
-
-    # check to see if there is a record with a matching name already in the list, return the position if there is.  
-    def checkForExistingPlayer(entryList, name:str):
-        # to keep the position in the array
-        counter:int = 0
-
-        #for every entry
-        for i in entryList:
-            
-            # go through every entry until a match is found
-            if i.name == name:
-
-                # return the position in the array
-                return counter
-            counter = counter + 1
-
-        # -1 being returned means that there was not a matching record found in the array 
-        return -1
-
-    def writeListToFile(entriesList:list[InfoSaving.Entry]):
-        textFormat = ""
-        with open(InfoSaving.textFileDirectory, "w") as textFile:
-            for i in entriesList:
-                textFormat = textFormat + i.name + ","
-                textFormat = textFormat + str(i.wins) + ","
-                textFormat = textFormat + str(i.losses) + ",\n"
-        
-            textFile.write(textFormat)
-
-        textFile.close()
-        
-        
-    def addScore(name:str, isWin:bool):
-        entriesList = InfoSaving.readFile(InfoSaving.textFileDirectory)
-
-        arrayPosition = InfoSaving.checkForExistingPlayer(entriesList, name) 
-
-        if (arrayPosition == -1):
-            newEntry:InfoSaving.Entry = InfoSaving.Entry()
-            newEntry.name = "input method unknown as of now"
-            entriesList.append(newEntry)
-            arrayPosition = len(entriesList) - 1
-
-        if (isWin):
-            entriesList[arrayPosition].wins = entriesList[arrayPosition].wins + 1
-        else:
-            entriesList[arrayPosition].losses = entriesList[arrayPosition].losses + 1
-        
-        InfoSaving.writeListToFile(entriesList)
-
-
-
-
-
 
 # Adding a test game into __main__ for testing purposes, this won't run when imported
-#if __name__ == "__main__":
-#    game: Game = Game()
-#    game.display()
-#    while (
-#        not game.is_won("x") and not game.is_won("o") and len(game.empty_space()) != 0
-#    ):
-#        print("Player X turn.")
-#        x = int(input("Input x: ")) - 1
-#        if x >= 3:
-#            x = 2
-#        elif x <= -1:
-#            x = 0
-#        y = int(input("Input y: ")) - 1
-#        if y >= 3:
-#            y = 2
-#        elif y <= -1:
-#            y = 0
-#        index = x + 3 * y
-#        game.perform_move(index, "x")
-#        game.display()
-#        if game.is_won("x") or len(game.empty_space()) == 0:
-#            break
-#        print("Player O turn.")
-#        x = int(input("Input x: ")) - 1
-#        if x >= 3:
-#            x = 2
-#        elif x <= -1:
-#            x = 0
-#        y = int(input("Input y: ")) - 1
-#        if y >= 3:
-#            y = 2
-#        elif y <= -1:
-#            y = 0
-#        index = x + 3 * y
-#        game.perform_move(index, "o")
-#        game.display()
-#        if game.is_won("x") or len(game.empty_space()) == 0:
-#            break
-#    print("Game over!")
-#    if game.is_won("x"):
-#        print("X wins!")
-#    elif game.is_won("o"):
-#        print("O wins!")
-#    else:
-#        print("Draw!")
-
-test:InfoSaving = InfoSaving()
-InfoSaving.textFileDirectory = "./game_engine/testfile.txt"
-test.textFileDirectory = "./game_engine/testfile.txt"
-test.addScore("a")
+if __name__ == "__main__":
+    game: Game = Game()
+    game.display()
+    while (
+        not game.is_won("x") and not game.is_won("o") and len(game.empty_space()) != 0
+    ):
+        print("Player X turn.")
+        x = int(input("Input x: ")) - 1
+        if x >= 3:
+            x = 2
+        elif x <= -1:
+            x = 0
+        y = int(input("Input y: ")) - 1
+        if y >= 3:
+            y = 2
+        elif y <= -1:
+            y = 0
+        index = x + 3 * y
+        game.perform_move(index, "x")
+        game.display()
+        if game.is_won("x") or len(game.empty_space()) == 0:
+            break
+        print("Player O turn.")
+        x = int(input("Input x: ")) - 1
+        if x >= 3:
+            x = 2
+        elif x <= -1:
+            x = 0
+        y = int(input("Input y: ")) - 1
+        if y >= 3:
+            y = 2
+        elif y <= -1:
+            y = 0
+        index = x + 3 * y
+        game.perform_move(index, "o")
+        game.display()
+        if game.is_won("x") or len(game.empty_space()) == 0:
+            break
+    print("Game over!")
+    if game.is_won("x"):
+        print("X wins!")
+    elif game.is_won("o"):
+        print("O wins!")
+    else:
+        print("Draw!")
